2fd596690b65d6960ade190ab2fd364530eb5afe
kimmingyeong225 <kred730036@gmail.com>
2025-05-14 11:23:52 +0900

    파일 설정

--- demo/src/main/java/com/fmp/demo/controller/TestController2.java
+++ demo/src/main/java/com/fmp/demo/controller/TestController2.java
@@ -14,15 +14,5 @@ public class TestController2 {
         return "index";  // src/main/resources/templates/index.html
     }
 
-//    @GetMapping("/match")
-//    public String matchResult(Model model) {
-//        // TODO: 실제 랜덤 매칭 로직을 서비스로 분리
-//        List<String> matchedPairs = List.of(
-//            "홍길동 ↔ 김철수",
-//            "이영희 ↔ 박민수",
-//            "최수민 ↔ 장예린"
-//        );
-//        model.addAttribute("pairs", matchedPairs);
-//        return "match-result";  // src/main/resources/templates/match-result.html
-//    }
+
 }
